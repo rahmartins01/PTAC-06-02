e80a37d8d7d3f94c4b0dd0897d698a47330aadfa
rahmartins01 <rayssamartins1256@gmail.com>
2024-03-13 10:52:58 -0400

    13/03/2024
    ata

---
+++ src/componentes/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return(
+        <footer>
+            <h1>Rayssa</h1>
+        </footer>
+    );
+}
---
+++ src/componentes/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return(
+        <Header>
+            <h1>PTS</h1>
+        </Header>
+    );
+}
